dd9b0b67f7397ee9b7af9a8ee116e90eaf327c7f
MujahidEmon <mujahidulislam.official@gmail.com>
2024-09-06 17:24:02 +0600

    first commit Ready to Dev

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import profile from '../../../images/profile.png'
+const Header = () => {
+    return (
+        <div className='flex justify-between items-center p-4 mx-8 '>
+            <h1 className="text-3xl text-center font-bold">SLASH Knowledeg Cafe</h1>
+            <img src={profile} alt="" />
+        </div>
+    );
+};
+
+export default Header;
